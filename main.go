80be39e70db73b6116b8d2a9e913bd016ef77342
Ville Puuska <puuskaville@gmail.com>
2023-11-26 16:34:15 +0200

    listen to a port and echo path

--- main.go
+++ main.go
@@ -1,7 +1,21 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"log"
+	"net/http"
+)
+
+const port string = "8080"
+
+func handleRoot(w http.ResponseWriter, r *http.Request) {
+	fmt.Println("Request path:")
+	fmt.Println(r.URL.Path)
+	fmt.Println()
+}
 
 func main() {
-	fmt.Println("Ready to start.")
+	fmt.Println("Listening on port", port)
+	http.HandleFunc("/", handleRoot)
+	log.Fatal(http.ListenAndServe(":"+port, nil))
 }
