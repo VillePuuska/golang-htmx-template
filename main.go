88c815b8b1664c65baa805b07ed1fdf33993e57c
Ville Puuska <puuskaville@gmail.com>
2023-12-07 17:28:21 +0200

    change printing html as string to executing template

--- main.go
+++ main.go
@@ -5,7 +5,6 @@ import (
 	"html/template"
 	"log"
 	"net/http"
-	"os"
 	"strings"
 	"time"
 )
@@ -18,22 +17,14 @@ func handleMissing(w http.ResponseWriter, r *http.Request) {
 	fmt.Println()
 	tmpl := template.Must(template.ParseFiles("pages/missing.html"))
 	tmpl.Execute(w, nil)
-	//file, err := os.ReadFile("pages/missing.html")
-	//if err != nil {
-	//	log.Fatal(err)
-	//}
-	//fmt.Fprintln(w, string(file))
 }
 
 func handleHome(w http.ResponseWriter, r *http.Request) {
 	fmt.Println("Request path:")
 	fmt.Println(r.URL.Path)
 	fmt.Println()
-	file, err := os.ReadFile("pages/home.html")
-	if err != nil {
-		log.Fatal(err)
-	}
-	fmt.Fprintln(w, string(file))
+	tmpl := template.Must(template.ParseFiles("pages/home.html"))
+	tmpl.Execute(w, nil)
 }
 
 func handleTime(w http.ResponseWriter, r *http.Request) {
